506da9ea44434f476a79342541588157c80d033f
bhardwajayush444 <89515899+bhardwajayush444@users.noreply.github.com>
2024-03-14 08:27:37 +0530

    CSV Support
    adding csv support

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -2,10 +2,12 @@ const { By,WebDriver } = require('selenium-webdriver');
 const r=require('selenium-webdriver')
 const Helper = require('../lib/Helper');
 class LoginPage{
+   
   
     static get userName(){return By.id('username')}
     static get password(){return By.id('password')}
     static get submit(){return By.id('submit')}
+    static get successFullLogin(){return By.xpath("//h1[text()='Logged In Successfully']")}
     
     constructor(driver){
         /** @type {WebDriver} */
--- tests/loginTest.spec.js
+++ tests/loginTest.spec.js
@@ -3,9 +3,14 @@ const Driver = require('../drivers/DriverManager');
 
 const { WebDriver } = require('selenium-webdriver');
 const LoginPage=require('../pages/LoginPage')
-
+const assert=require('assert')
 const Helper = require('../lib/Helper');
+const fs=require('fs')
 
+const addContext=require('mochawesome/addContext')
+const CSVReader = require('../lib/CSVReader'); 
+const logger = require('../lib/loggers');
+const path = require('path');
 describe('Example Test Suite', function () {
 
     /**  @type {WebDriver} */
@@ -17,24 +22,65 @@ describe('Example Test Suite', function () {
      /** @type {Helper} */
     let helper;
     this.timeout(500000)
+   
 
-    before(async function () {
+    beforeEach(async function () {
       driver = await Driver.initialize();
       loginPage = new LoginPage(driver);
       helper=new Helper(driver);
+      const csvReader = new CSVReader(path.resolve(__dirname, 'testData.csv'));
+      const testData = await csvReader.readAllData();
+      // for(let data of testData){
+      //   logger.info(data.username)
+      //   logger.info(data.password)
+      //   logger.info(data.expected_result)
+      // }
+      
       });
     
-      after(async function () {
-        await Driver.quit(driver);
-      });
     
 
-    it('Run test',async function(){
+    it('Successfull Login',async function(){
     
+        logger.info('reading first row')
+        const csvReader = new CSVReader(path.resolve(__dirname, 'testData.csv'));
+        const testData = await csvReader.readRow(1)
+        logger.info(testData.username)
         await loginPage.login('student','Password123')
-        await helper.sleep(8000)
+        await helper.sleep(5000)
+        const successFullLoginDisplayed=await driver.findElement(LoginPage.successFullLogin).isDisplayed()
+        assert.strictEqual(successFullLoginDisplayed,true)
+        
    
 
     })
-  
+   
+    it('Unsuccessfull Login',async function(){
+    
+      await loginPage.login('student','Password123')
+      await helper.sleep(5000)
+      const successFullLoginDisplayed=await driver.findElement(LoginPage.successFullLogin).isDisplayed()
+      assert.strictEqual(successFullLoginDisplayed,false)
+      
+ 
+
+  })
+ 
+
+    afterEach(async function(){
+      
+      if(this.currentTest.state == 'failed'){
+          let imageFileName = this.currentTest.title + '.jpeg';
+          
+          driver.takeScreenshot().then(
+              function(image){
+                  require('fs').writeFileSync('./screenshots/' + imageFileName, image, 'base64')
+              }
+          )
+          addContext(this,'Following comes the failed test image')
+          addContext(this, '../screenshots/' + imageFileName)
+      }
+      await Driver.quit(driver);
+     
+  })
 })  
